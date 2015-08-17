baff170c60cb2906a6b52300157e0435925261fa
sarbyn <sarbyn@gmail.com>
2015-08-17 16:38:24 +0200

    Init BT connection

--- android/app/src/main/java/com/sarbyn/androidbt/MainActivity.java
+++ android/app/src/main/java/com/sarbyn/androidbt/MainActivity.java
@@ -1,38 +1,89 @@
 package com.sarbyn.androidbt;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Set;
 
 
 public class MainActivity extends Activity {
+
+    public static final String LOG_TAG = "ANDROID-BT";
+    public static final String BLUETOOTH_DEVICE_NAME = "HC-06";
+
+    BluetoothAdapter adapter = null;
+    BluetoothDevice arduinoDevice = null;
+
+    Button connectButton, disconnectButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        connectButton = (Button) findViewById(R.id.button_connect);
+        connectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setupBTConnection();
+                openBTConnection();
+            }
+        });
+
+        disconnectButton = (Button) findViewById(R.id.button_disconnect);
+        disconnectButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                closeBTConnection();
+            }
+        });
+
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private void setupBTConnection() {
+        adapter = BluetoothAdapter.getDefaultAdapter();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (adapter == null) {
+            Toast.makeText(this, getString(R.string.bluetooth_unavailable), Toast.LENGTH_LONG).show();
+            return;
         }
 
-        return super.onOptionsItemSelected(item);
+        if (!adapter.isEnabled()) {
+            startActivityForResult(new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE), 0);
+            return;
+        }
+
+        Set<BluetoothDevice> pairedDevices = adapter.getBondedDevices();
+        for(BluetoothDevice device: pairedDevices) {
+            Log.d(LOG_TAG, device.getName());
+            if (device.getName().equals(BLUETOOTH_DEVICE_NAME)) {
+                arduinoDevice = device;
+            }
+        }
+
+        if (arduinoDevice == null) {
+            Toast.makeText(this, getString(R.string.no_arduino_device_found), Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, getString(R.string.arduino_device_found), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void openBTConnection() {
+
+    }
+
+    private void closeBTConnection() {
+        Toast.makeText(this, getString(R.string.bluetooth_disconnected), Toast.LENGTH_LONG).show();
     }
 }
